10eea078e28e19570cf4a692697812bd1f731efa
Marsid Zyberi <39383860+marsidi11@users.noreply.github.com>
2024-04-09 15:18:56 +0200

    add event listener if element exists

--- assets/frontend/script.js
+++ assets/frontend/script.js
@@ -1,31 +1,35 @@
 var clickCount = 1;
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('calculate').addEventListener('click', function() {
-        var x = document.getElementById('x').value;
-        var y = document.getElementById('y').value;
-        var z = document.getElementById('z').value;
+    var calculateButton = document.getElementById('calculate');
+    
+    if (calculateButton) {
+        calculateButton.addEventListener('click', function() {
+            var x = document.getElementById('x').value;
+            var y = document.getElementById('y').value;
+            var z = document.getElementById('z').value;
 
-        if (!x || !y || !z) {
-            alert('Ploteso Fushat');
-        } else if (x <= 0 || y <= 0 || z <= 0) {
-            alert('Vlerat duhet te jene pozitive');
-        } else {
-            var result = x * y - z;
-            document.getElementById('output').value = result;
+            if (!x || !y || !z) {
+                alert('Ploteso Fushat');
+            } else if (x <= 0 || y <= 0 || z <= 0) {
+                alert('Vlerat duhet te jene pozitive');
+            } else {
+                var result = x * y - z;
+                document.getElementById('output').value = result;
 
-            var data = {
-                'action': 'update_click_count',
-                'click_count': clickCount,
-            };
+                var data = {
+                    'action': 'update_click_count',
+                    'click_count': clickCount,
+                };
 
-            jQuery.post(my_script_vars.ajaxurl, data, function (response) {
-                console.log('Response from server: ' + response.data);
-            }).fail(function (xhr, status, error) {
-                console.log('Error: ' + error + ' with status: ' + status + ' with xhr: ' + xhr.responseText);
-            });
+                jQuery.post(my_script_vars.ajaxurl, data, function (response) {
+                    console.log('Response from server: ' + response.data);
+                }).fail(function (xhr, status, error) {
+                    console.log('Error: ' + error + ' with status: ' + status + ' with xhr: ' + xhr.responseText);
+                });
 
-        }
+            }
 
-    });
+        });
+    }
 });
